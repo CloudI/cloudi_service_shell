60c617d3e94bb241083eb3a907423a4d7aabf9aa
Michael Truog <mjtruog@protonmail.com>
2019-11-28 16:22:50 -0800

    Version 1.8.0.

---
+++ mix.exs
@@ -0,0 +1,47 @@
+#-*-Mode:elixir;coding:utf-8;tab-width:2;c-basic-offset:2;indent-tabs-mode:()-*-
+# ex: set ft=elixir fenc=utf-8 sts=2 ts=2 sw=2 et nomod:
+
+defmodule CloudIServiceShell do
+  use Mix.Project
+
+  def project do
+    [app: :cloudi_service_shell,
+     version: "1.8.0",
+     language: :erlang,
+     erlc_options: [
+       :debug_info,
+       :warnings_as_errors,
+       :strict_validation,
+       :warn_bif_clash,
+       :warn_deprecated_function,
+       :warn_export_all,
+       :warn_export_vars,
+       :warn_exported_vars,
+       :warn_obsolete_guard,
+       :warn_shadow_vars,
+       :warn_unused_import,
+       :warn_unused_function,
+       :warn_unused_record,
+       :warn_unused_vars],
+     description: description(),
+     package: package(),
+     deps: deps()]
+  end
+
+  defp deps do
+    [{:cloudi_core, "~> 1.8.0"}]
+  end
+
+  defp description do
+    "Erlang/Elixir Cloud Framework Shell Service"
+  end
+
+  defp package do
+    [files: ~w(src doc rebar.config README.markdown),
+     maintainers: ["Michael Truog"],
+     licenses: ["MIT"],
+     links: %{"Website" => "https://cloudi.org",
+              "GitHub" => "https://github.com/CloudI/" <>
+                          "cloudi_service_shell"}]
+   end
+end
